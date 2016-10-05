4ba477f68795a156f70ab15efdf166fa2744d3c1
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-10-05 19:57:29 +0900

    Add easy function of parse

--- Sources/Protocol.swift
+++ Sources/Protocol.swift
@@ -1,5 +1,7 @@
 //  Copyright (C) 2016 Yoshiki Kudo. All rights reserved.
 
+import Either
+
 
 // MARK: - ParserProtocol
 
@@ -7,6 +9,18 @@ public protocol ParserProtocol {
 	associatedtype Targets: Collection
 	associatedtype Tree
 	
-	func parse<A>(_ targets: Targets, at index: Targets.Index, ifSuccess: (Tree, Targets.Index) -> A, ifFailure: (ParsingError<Targets.Index>) -> A) -> A
+	func parse<A>(_ input: Targets, at index: Targets.Index, ifSuccess: (Tree, Targets.Index) -> A, ifFailure: (ParsingError<Targets.Index>) -> A) -> A
 }
 
+extension ParserProtocol {
+	public func parse(_ input: Targets) -> Either<ParsingError<Targets.Index>, Tree> {
+		return parse(input, at: input.startIndex,
+			ifSuccess: { tree, index in
+				return index == input.endIndex
+					? Either.right(tree)
+					: Either.left(ParsingError(index: index, reason: "index is not input.endIndex"))
+				},
+			ifFailure: Either.left
+		)
+	}
+}
