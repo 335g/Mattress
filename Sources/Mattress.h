acd2184faee9ddc0d1bd8c56986b56df54e92dde
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-10-05 19:36:28 +0900

    First commit

---
+++ Sources/Mattress.h
@@ -0,0 +1,19 @@
+//
+//  Mattress.h
+//  Mattress
+//
+//  Created by Yoshiki Kudo on 2016/10/05.
+//  Copyright © 2016年 Yoshiki Kudo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Mattress.
+FOUNDATION_EXPORT double MattressVersionNumber;
+
+//! Project version string for Mattress.
+FOUNDATION_EXPORT const unsigned char MattressVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mattress/PublicHeader.h>
+
+
