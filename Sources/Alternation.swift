57494afa14db7dfeb5a5f5906a1530645f9931b9
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-10-07 00:54:26 +0900

    Add AltParser

---
+++ Sources/Alternation.swift
@@ -0,0 +1,30 @@
+//  Copyright (C) 2016 Yoshiki Kudo. All rights reserved.
+
+import Either
+
+
+// MARK: - AltParser
+
+public struct AltParser<P1, P2> where P1: ParserProtocol, P2: ParserProtocol, P1.Targets == P2.Targets {
+	let this: P1
+	let other: P2
+}
+
+// AltParser : ParserProtocol
+
+extension AltParser: ParserProtocol {
+	public typealias Targets = P1.Targets
+	public typealias Tree = Either<P1.Tree, P2.Tree>
+	
+	public func parse<A>(_ input: P1.Targets, at index: P1.Targets.Index, ifSuccess: (Either<P1.Tree, P2.Tree>, P1.Targets.Index) -> A, ifFailure: (ParsingError<P1.Targets.Index>) -> A) -> A {
+		return this.parse(input, at: index,
+			ifSuccess: { tree, index in ifSuccess(Either.left(tree), index) },
+			ifFailure: { _ in
+				return self.other.parse(input, at: index,
+					ifSuccess: { tree, index in ifSuccess(Either.right(tree), index) },
+					ifFailure: ifFailure
+				)
+			}
+		)
+	}
+}
