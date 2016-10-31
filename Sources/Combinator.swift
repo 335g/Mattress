dba7a971654016d4500ad65be45728be11562770
Yoshiki Kudo <actionstar619@yahoo.co.jp>
2016-11-01 01:50:45 +0900

    between(_:_:) -> sandwiched(_:_:)

--- Sources/Combinator.swift
+++ Sources/Combinator.swift
@@ -1,13 +1,13 @@
 //  Copyright (C) 2016 Yoshiki Kudo. All rights reserved.
 
-
-
-public func between<P1, P2, P3>(_ open: P1, and close: P2) -> (P3) -> IgnoreParser<ConcatParser<P1, P3>, MapParser<P2, P3.Tree>> where
-	P1: ParserProtocol,
-	P2: ParserProtocol,
-	P3: ParserProtocol,
-	P1.Targets == P2.Targets,
-	P2.Targets == P3.Targets
-{
-	return { open *> $0 <* close }
+extension ParserProtocol {
+	public func sandwiched<P1, P2>(between open: P1, and close: P2) -> IgnoreParser<ConcatParser<P1, Self>, MapParser<P2, Self.Tree>>
+		where
+			P1: ParserProtocol,
+			P2: ParserProtocol,
+			P1.Targets == Self.Targets,
+			P2.Targets == Self.Targets
+	{
+		return open *> self <* close
+	}
 }
